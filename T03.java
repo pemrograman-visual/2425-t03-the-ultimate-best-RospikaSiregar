1e72e2d4dece9f73f1b1c5b7b02c9e70bbd10bc3
RospikaSiregar <rospikasarahsiregar@gmail.com>
2024-10-04 11:17:22 +0700

    telah menyelesaikan T03

--- T03.java
+++ T03.java
@@ -1,5 +1,5 @@
-// 12S24008 - Rospika Sarah Yosefin Siregar
-// 12S24013 - Adryan Julianto Panjaitan
+// 12S24008 - ROSPIKA SARAH YOSEFIN SIREGAR
+// 12S24013 - ADRYAN JULIANTO PANJAITAN
 
 import java.util.*;
 import java.lang.Math;
@@ -12,53 +12,55 @@ public class T03 {
         int tahunterbit, stokbuku;
         double hargapembelian, minimummargin, rating, diskon;
 
-        iSBN = input.nextLine();
-        while (!iSBN.equals("---")) {
-            judulbuku = input.nextLine();
-            penulis = input.nextLine();
-            tahunterbit = Integer.parseInt(input.nextLine());
-            penerbit = input.nextLine();
-            formatbukuelektronik = input.nextLine();
-            hargapembelian = Double.parseDouble(input.nextLine());
-            minimummargin = Double.parseDouble(input.nextLine());
-            diskon = minimummargin / hargapembelian * -1;
-            if (diskon >= 0.4) {
-                kategoridiskon = "Once in a lifetime";
+        do {
+            iSBN = input.nextLine();
+            if (iSBN.equals("---")) {
             } else {
-                if (diskon > 0.2) {
-                    kategoridiskon = "Never come twice";
+                judulbuku = input.nextLine();
+                penulis = input.nextLine();
+                tahunterbit = input.nextInt();
+                penerbit = input.nextLine();
+                formatbukuelektronik = input.nextLine();
+                hargapembelian = input.nextDouble();
+                minimummargin = input.nextDouble();
+                diskon = minimummargin / hargapembelian * -1;
+                if (diskon > 0.4) {
+                    kategoridiskon = "Once in a lifetime";
                 } else {
-                    kategoridiskon = "No regret";
-                }
-            }
-            stokbuku = Integer.parseInt(input.nextLine());
-            rating = Double.parseDouble(input.nextLine());
-            if (rating >= 4.7 && rating <= 5.0) {
-                kategori = "Best Pick";
-            } else {
-                if (rating >= 4.5 && rating < 4.7) {
-                    kategori = "Must Read";
-                } else {
-                    if (rating >= 4.0 && rating < 4.5) {
-                        kategori = "Recommended";
+                    if (diskon > 0.2) {
+                        kategoridiskon = "Never come twice";
                     } else {
-                        if (rating >= 3.0 && rating < 4.0) {
-                            kategori = "Average";
+                        kategoridiskon = "No regret";
+                    }
+                }
+                stokbuku = input.nextInt();
+                rating = input.nextDouble();
+                if (rating >= 4.7 && rating <= 5.0) {
+                    kategori = "Best Pick";
+                } else {
+                    if (rating >= 4.5 && rating < 4.7) {
+                        kategori = "Must Read";
+                    } else {
+                        if (rating >= 4.0 && rating < 4.5) {
+                            kategori = "Recommended";
                         } else {
-                            if (rating < 3.0 && rating >= 0) {
-                                kategori = "Low";
+                            if (rating >= 3.0 && rating < 4.0) {
+                                kategori = "Average";
+                            } else {
+                                if (rating < 3.0 && rating >= 0) {
+                                    kategori = "Low";
+                                }
                             }
                         }
                     }
                 }
+                if (diskon > 0.4 && rating >= 4.7) {
+                    tub = "The ultimate best";
+                } else {
+                    tub = "---";
+                }
+                System.out.println(iSBN + "|" + judulbuku + "|" + penulis + "|" + tahunterbit + "|" + penerbit + "|" + formatbukuelektronik + "|" + hargapembelian + "|" + minimummargin + "|" + stokbuku + "|" + rating + "|" + kategori + "|" + kategoridiskon + "|" + tub);
             }
-            if (rating >= 4.7 && diskon > 0.4) {
-                tub = "The ultimate best";
-            } else {
-                tub = "---";
-            }
-            System.out.println(iSBN + "|" + judulbuku + "|" + penulis + "|" + tahunterbit + "|" + penerbit + "|" + formatbukuelektronik + "|" + hargapembelian + "|" + minimummargin + "|" + stokbuku + "|" + rating + "|" + kategori + "|" + tub);
-        }
+        } while (!iSBN.equals("---"));
     }
 }
-
